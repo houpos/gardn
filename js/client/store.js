03339ad99d0d8062a713b9f7fac97fb4b54e8b2b
Meredyth Houpos <meredyth.houpos@gmail.com>
2019-11-17 16:07:03 -0500

    added more models

--- js/client/store.js
+++ js/client/store.js
@@ -4,87 +4,75 @@ import thunkMiddleware from 'redux-thunk' // https://github.com/gaearon/redux-th
 import axios from 'axios'
 
  const arrOfModels = [
+   {
+     title: 'Flower',
+     source: require('./res/flower/cartoon_flower.obj'),
+     thumbnail: require('./res/flower/flower.png'),
+     resources: [require('./res/flower/cartoon_flower.mtl')],
+     scale: [1, 1, 1],
+     type: 'OBJ'
+   },
+   {
+     title: 'Primrose',
+     source: require('./res/PrimroseP_obj/PrimroseP.obj'),
+     thumbnail: require('./res/PrimroseP_obj/primrose.png'),
+     resources: [
+       require('./res/PrimroseP_obj/PrimroseP.mtl'),
+       require('./res/PrimroseP_obj/PRIM1P.png'),
+       require('./res/PrimroseP_obj/PRIM1ST.png'),
+       require('./res/PrimroseP_obj/PRIM1L2.png'),
+       require('./res/PrimroseP_obj/PRIM1L3.png'),
+       require('./res/PrimroseP_obj/PRIMsoil.png'),
+       require('./res/PrimroseP_obj/vase.png')
+     ],
+     scale: [0.3, 0.3, 0.3],
+     type: 'OBJ'
+   },
    //  {
-   //    title: 'Flower Pot',
-   //    source: require('../res/plant_flowerpot/flowers+pot.obj'),
-   //    thumbnail: require('../res/placeholder-square.jpg'),
-   //    resources: [],
-   //    position: [-0.5, 0.5, -1],
-   //    scale: [0.2, 0.2, 0.2],
+   //    title: 'Rose',
+   //    source: require('./res/rose/rose.obj'),
+   //    thumbnail: require('./res/placeholder-square.jpg'),
+   //    resources: [
+   //      require('./res/rose/rose.mtl'),
+   //      require('./res/rose/Files/rose_texture.jpg')
+   //    ],
+   //    scale: [0.1, 0.1, 0.1],
    //    type: 'OBJ'
    //  },
    {
-     title: 'Dog',
-     source: require('./res/pet_dog1/Dog.obj'),
-     thumbnail: require('./res/placeholder-square.jpg'),
+     title: 'Birch Tree',
+     source: require('./res/1_birchSmall/1_birchSmall_fan.obj'),
+     thumbnail: require('./res/1_birchSmall/birch_tree.png'),
      resources: [
-       require('./res/pet_dog1/Dog.mtl'),
-       require('./res/pet_dog1/Diffuse.png'),
-       require('./res/pet_dog1/Specular.png')
+       require('./res/1_birchSmall/1_birchSmall_fan.mtl'),
+       require('./res/1_birchSmall/1_birch_tree_fan.png'),
+       require('./res/1_birchSmall/birch-leaf-diff.png'),
+       require('./res/1_birchSmall/birkenstamm-diffuse.png')
      ],
-     position: [0, 0.5, 0],
      scale: [0.2, 0.2, 0.2],
      type: 'OBJ'
    },
    {
      title: 'Bush',
      source: require('./res/plant_bush1/bush.obj'),
-     thumbnail: require('./res/placeholder-square.jpg'),
+     thumbnail: require('./res/plant_bush1/bush.png'),
      resources: [
        require('./res/plant_bush1/bush.mtl'),
        require('./res/plant_bush1/bush01.png'),
        require('./res/plant_bush1/bush02.png'),
        require('./res/plant_bush1/bush03.png')
      ],
-     position: [0, 0.5, 0],
      scale: [0.2, 0.2, 0.2],
      type: 'OBJ'
-   },
-   {
-     title: 'Chair',
-     source: require('./res/furniture_chair1/OBJ.obj'),
-     thumbnail: require('./res/placeholder-square.jpg'),
-     resources: [
-       require('./res/furniture_chair1/Dirt.jpg'),
-       require('./res/furniture_chair1/Pillow.jpg'),
-       require('./res/furniture_chair1/Wood.jpg')
-     ],
-     position: [0, 0.5, 0],
-     scale: [0.2, 0.2, 0.2],
-     type: 'OBJ'
-   },
-   {
-     title: 'Smiley',
-     source: require('./res/emoji_smile/emoji_smile.vrx'),
-     thumbnail: require('./res/placeholder-square.jpg'),
-     resources: [
-       './res/emoji_smile/emoji_smile_diffuse.png',
-       './res/emoji_smile/emoji_smile_normal.png',
-       './res/emoji_smile/emoji_smile_specular.png'
-     ],
-     position: [0, 0.5, 0],
-     scale: [0.2, 0.2, 0.2],
-     type: 'VRX'
    }
- ]
-
- const modelNames = [
-   {
-     title: 'Dog',
-     thumbnail: require('./res/placeholder-square.jpg')
-   },
-   {
-     title: 'Bush',
-     thumbnail: require('./res/placeholder-square.jpg')
-   },
-   {
-     title: 'Chair',
-     thumbnail: require('./res/placeholder-square.jpg')
-   },
-   {
-     title: 'Smiley',
-     thumbnail: require('./res/placeholder-square.jpg')
-   }
+  //  {
+  //    title: 'Fence',
+  //    source: require('./res/fence/Fence.obj'),
+  //    thumbnail: require('./res/fence/fence.png'),
+  //    resources: [require('./res/fence/Fence.mtl')],
+  //    scale: [0.1, 0.1, 0.1],
+  //    type: 'OBJ'
+  //  }
  ]
 
 // Add any initial state properties. Use Tom's First Law
@@ -100,9 +88,9 @@ const GET_MODELS = 'GET_MODELS'
 const GET_MODEL_NAMES = 'GET_MODEL_NAMES'
 
 // ACTION CREATORS
-const gotModels = allModels => ({
+export const getModels = () => ({
   type: GET_MODELS,
-  allModels
+  arrOfModels
 })
 
 export const addModel = model => ({
@@ -110,20 +98,13 @@ export const addModel = model => ({
   model
 })
 
-export const gotModelNames = () => ({
-  type: GET_MODEL_NAMES,
-  modelNames
-})
-
-// THUNKS
-export const getModels = () => {
-  return async dispatch => {
-    try {
-      const data = await [...arrOfModels]
-      return dispatch(gotModels(data))
-    } catch (error) {
-      console.error(error)
-    }
+export const gotModelNames = () => {
+  const names = arrOfModels.map((model) => {
+    return {title: model.title, thumbnail: model.thumbnail }
+  })
+  return {
+    type: GET_MODEL_NAMES,
+    modelNames: names
   }
 }
 
